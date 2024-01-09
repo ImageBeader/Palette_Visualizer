dcda9057c3d25f69017ec0ab3dab776d2425955d
Isekaijin Rance <Isekaijin.tech@gmail.com>
2024-01-09 10:41:21 -0500

    Created MainPVWindow to handle the main window.

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 # This Python file uses the following encoding: utf-8
 import sys
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow,QLabel
 
+class MainPVWindow(QMainWindow):
+    def __init__(self):
+        super(MainPVWindow, self).__init__()
 
+        self.setWindowTitle("Palette Visualizer")
+        self.setGeometry(500,270     #Window position
+                        ,960,540     #Window size
+                        )
 
+        label = QLabel("Pallete Visualizer",self)
+        label.setGeometry(110,50
+                         ,200,100
+                         )
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    window = QMainWindow()
-    window.setWindowTitle("Palette Visualizer")
-    window.setGeometry(500,270      #Window position
-                      ,960,540     #Window size
-                      )
-
+    window = MainPVWindow()
     window.show()
-
 
     sys.exit(app.exec())
